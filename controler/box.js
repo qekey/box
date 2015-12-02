4fd50f00e275e8b78dc018b9035348b2ef547d14
zuaa <zuaa@163.com>
2015-12-02 22:31:05 +0800

    增加部分功能连接

--- controler/box.js
+++ controler/box.js
@@ -2,12 +2,21 @@ var c = require('../config/config');
 var log = eval("c.config." + c.config.config + ".log"); 
 var mfs = require('../data/mfs');
 module.exports = function(app) {
-	try {
+	 
 		app.get('/', function(request, response) {  
-			log.debug('hi debug, i am here  %s.',app);
+			log.debug('hi /, i am here  %s.',app);
 			response.render('index.ejs', {"mfses":mfs.list});
 		});
-	} catch (e) {
-		console.log(e)
-	} 
+		app.get('/updateLog', function(request, response) {  
+			log.debug('hi updateLog, i am here  %s.',app);
+			response.render('index.ejs', {"mfses":mfs.list});
+		});
+		app.get('/mainAction', function(request, response) {  
+			log.debug('hi mainAction, i am here  %s.',app);
+			response.render('index.ejs', {"mfses":mfs.list});
+		});
+		app.get('/caidana', function(request, response) {  
+			log.debug('hi caidana, i am here  %s.',app);
+			response.render('index.ejs', {"mfses":mfs.list});
+		}); 
 }
